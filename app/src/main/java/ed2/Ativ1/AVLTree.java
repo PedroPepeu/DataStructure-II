2a92764e8e1e9d2000d120baa0dda7a98b3732a3
PedroPepeu <pedrosteamgtav@gmail.com>
2023-08-20 20:33:21 -0300

    AVLtree part 1 finished

--- app/src/main/java/ed2/Ativ1/AVLTree.java
+++ app/src/main/java/ed2/Ativ1/AVLTree.java
@@ -1,12 +1,15 @@
 package ed2.Ativ1;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class AVLTree<T extends Comparable <T>> {
 
     private AVLNode<T> root;
 
-    // inserir
-    // pesseioemordem
-    // passeiopornivel
+    // inserir CHECK
+    // pesseioemordem CHECK
+    // passeiopornivel CHECK
 
     public AVLTree(AVLNode<T> root, Integer size) {
         this.root = root;
@@ -19,6 +22,7 @@ public class AVLTree<T extends Comparable <T>> {
     private AVLNode<T> insert(T info, AVLNode<T> currentNode) {
         if(currentNode == null) {
             AVLNode<T> newNode = new AVLNode<T>(info);
+            newNode.setFatBal(0);
             return newNode;
         }
 
@@ -30,7 +34,58 @@ public class AVLTree<T extends Comparable <T>> {
         if(cmp < 0) {
             leftSubtree = insert(info, currentNode.getLeft());
             currentNode.setLeft(leftSubtree);
+            currentNode.setFatBal(currentNode.getFatBal() - 1);
+        } 
+        else {
+            rightSubtree = insert(info, currentNode.getRight());
+            currentNode.setRight(rightSubtree);
+            currentNode.setFatBal(currentNode.getFatBal() + 1);
         }
+
+        return currentNode;
+    }
+
+    public void InOrder() {
+        InOrder(getRoot());
+        return;
+    }
+
+    private void InOrder(AVLNode<T> currentNode) {
+        if(currentNode == null) return;
+
+        InOrder(currentNode.getLeft());
+
+        System.out.printf("%s\n", currentNode.getInfo().toString());
+
+        InOrder(currentNode.getRight());
+
+        return;
+    }
+
+    public void BreadthFirst() {
+        BreadthFirst(getRoot());
+        return;
+    }
+
+    private void BreadthFirst(AVLNode<T> currentNode) {
+        if(currentNode == null) return;
+        
+        Queue<AVLNode<T>> Qnodes = new LinkedList<AVLNode<T>>();
+        Qnodes.add(currentNode);
+        int k = 1, s = 1;
+
+        while(!Qnodes.isEmpty()) {
+            currentNode = Qnodes.poll();
+            System.out.printf("%s ", currentNode.getInfo().toString());
+            if(k == 0) {
+                System.out.println();
+                k = s*=2;
+            } else k--;
+            if(currentNode.getLeft() != null) Qnodes.add(currentNode.getLeft());
+            if(currentNode.getRight() != null) Qnodes.add(currentNode.getRight());
+        }
+
+        return;
     }
 
     public AVLNode<T> getRoot() {
